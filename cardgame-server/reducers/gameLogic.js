af9d7ea141738f594e4d210cd86d9b1e5be4ad9e
Valian <jakub.skalecki@gmail.com>
2016-03-21 19:37:24 +0100

    game state structure

---
+++ cardgame-server/reducers/gameLogic.js
@@ -0,0 +1,37 @@
+/*
+var state = {
+    shared = {
+        board: [
+            [4, 8, 13],
+            [5, 6, 12, 23],
+            [1],
+            [54]
+        ],
+        players: [
+            {
+                name: 'First',
+                cardsCount: 5,
+                penalty: 23
+            },
+            {
+                name: 'Second',
+                cardsCount: 5,
+                penalty: 23
+            }
+        ],
+        state: {
+            playersReady: [0],
+            phase: 'selecting/selected/...'
+        }
+    },
+    playersPrivate: [
+        {
+            cardsOnHand: [2, 5, 6, 6, 7]
+        },
+        {
+            cardsOnHand: [45, 65, 65, 34, 23]
+        }
+    ]
+}
+
+ */
